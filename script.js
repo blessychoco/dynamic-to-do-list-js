f99ed53d6d71fbccf492c1351af1a23e6ae26118
blessychoco <ezeblessing099@gmail.com>
2025-10-07 18:13:38 +0100

    Fix classList.add for remove button

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Create a remove button
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
 
         // Assign onclick event to remove button
         removeButton.onclick = function() {
